23dae4b0800dbf2efb4e78cc15003d449c8cbda7
Assilvera <124600969+Assilvera@users.noreply.github.com>
2025-11-28 21:02:29 -0500

    fix: update public page

--- src/pages/public/index.tsx
+++ src/pages/public/index.tsx
@@ -1,15 +1,12 @@
 import Login from "../../components/login";
 
 const Public = () => {
-  return (
-    <>
-      <h1 style={{ color: "purple", textAlign: "center" }}>
-        🚀 Deploy Test – Firebase CI/CD funcionando ✔️
-      </h1>
-
-      <Login />
-    </>
-  );
-};
+    return (
+        <>
+            <h1>Public</h1>
+            <Login />
+        </>
+    )
+}
 
 export default Public;
